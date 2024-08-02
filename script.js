f5b87cd1c6b8d37c27f16b8dfaa3766bbe75e410
slimeplayzgamesany <140193701+slimeplayzgamesany@users.noreply.github.com>
2024-08-02 16:51:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,724 @@
+let note1 = document.getElementById("1");
+let note2 = document.getElementById("2");
+let note3 = document.getElementById("3");
+let note4 = document.getElementById("4");
+let note5 = document.getElementById("5");
+let note6 = document.getElementById("6");
+let note7 = document.getElementById("7");
+let note8 = document.getElementById("8");
+let note9 = document.getElementById("9");
+let note10 = document.getElementById("10");
+let note11 = document.getElementById("11");
+let note12 = document.getElementById("12");
+let note13 = document.getElementById("13");
+let note14 = document.getElementById("14");
+let note15 = document.getElementById("15");
+let note16 = document.getElementById("16");
+let note17 = document.getElementById("17");
+let note18 = document.getElementById("18");
+let note19 = document.getElementById("19");
+let note20 = document.getElementById("20");
+let note21 = document.getElementById("21");
+let note22 = document.getElementById("22");
+let note23 = document.getElementById("23");
+let note24 = document.getElementById("24");
+let note25 = document.getElementById("25");
+let note26 = document.getElementById("26");
+let note27 = document.getElementById("27");
+let note28 = document.getElementById("28");
+let note29 = document.getElementById("29");
+let note30 = document.getElementById("30");
+let note31 = document.getElementById("31");
+let note32 = document.getElementById("32");
+let note33 = document.getElementById("33");
+let note34 = document.getElementById("34");
+let note35 = document.getElementById("35");
+let note36 = document.getElementById("36");
+let note37 = document.getElementById("37");
+let note38 = document.getElementById("38");
+let note39 = document.getElementById("39");
+let note40 = document.getElementById("40");
+let note41 = document.getElementById("41");
+let note42 = document.getElementById("42");
+let note43 = document.getElementById("43");
+let note44 = document.getElementById("44");
+let note45 = document.getElementById("45");
+let note46 = document.getElementById("46");
+let note47 = document.getElementById("47");
+let note48 = document.getElementById("48");
+let note49 = document.getElementById("49");
+let note50 = document.getElementById("50");
+let note51 = document.getElementById("51");
+let note52 = document.getElementById("52");
+let note53 = document.getElementById("53");
+let note54 = document.getElementById("54");
+let note55 = document.getElementById("55");
+let note56 = document.getElementById("56");
+let a1=document.getElementById("a-1");
+let a2=document.getElementById("a-2");
+let a3=document.getElementById("a-3");
+let a4=document.getElementById("a-4");
+let a5=document.getElementById("a-5");
+let a6=document.getElementById("a-6");
+let a7=document.getElementById("a-7");
+let a8=document.getElementById("a-8");
+let a9=document.getElementById("a-9");
+let a10=document.getElementById("a-10");
+let a11=document.getElementById("a-11");
+let a12=document.getElementById("a-12");
+let a13=document.getElementById("a-13");
+let a14=document.getElementById("a-14");
+let a15=document.getElementById("a-15");
+let a16=document.getElementById("a-16");
+let a17=document.getElementById("a-17");
+let a18=document.getElementById("a-18");
+let a19=document.getElementById("a-19");
+let a20=document.getElementById("a-20");
+let a21=document.getElementById("a-21");
+let a22=document.getElementById("a-22");
+let a23=document.getElementById("a-23");
+let a24=document.getElementById("a-24");
+let a25=document.getElementById("a-25");
+let a26=document.getElementById("a-26");
+let a27=document.getElementById("a-27");
+let a28=document.getElementById("a-28");
+let a29=document.getElementById("a-29");
+let a30=document.getElementById("a-30");
+let a31=document.getElementById("a-31");
+let a32=document.getElementById("a-32");
+let a33=document.getElementById("a-33");
+let a34=document.getElementById("a-34");
+let a35=document.getElementById("a-35");
+let a36=document.getElementById("a-36");
+let a37=document.getElementById("a-37");
+let a38=document.getElementById("a-38");
+let a39=document.getElementById("a-39");
+let a40=document.getElementById("a-40");
+let a41=document.getElementById("a-41");
+let a42=document.getElementById("a-42");
+let a43=document.getElementById("a-43");
+let a44=document.getElementById("a-44");
+let a45=document.getElementById("a-45");
+let a46=document.getElementById("a-46");
+let a47=document.getElementById("a-47");
+let a48=document.getElementById("a-48");
+let a49=document.getElementById("a-49");
+let a50=document.getElementById("a-50");
+let a51=document.getElementById("a-51");
+let a52=document.getElementById("a-52");
+let a53=document.getElementById("a-53");
+let a54=document.getElementById("a-54");
+let a55=document.getElementById("a-55");
+let a56=document.getElementById("a-56");
+console.log(a1);
+
+note1.addEventListener("mousedown",(event)=>{
+    note1.style.backgroundColor="red";
+    a1.src=`key01.mp3`;  
+}) 
+
+note1.addEventListener("mouseup",(event)=>{
+    note1.style.backgroundColor="white";
+    console.log(event);
+}) 
+note2.addEventListener("mousedown",(event)=>{
+    note2.style.backgroundColor="orange";
+    console.log(event);
+    a2.src=`key02.mp3`;  
+}) 
+
+note2.addEventListener("mouseup",(event)=>{
+    note2.style.backgroundColor="white";
+    console.log(event);
+      
+}) 
+
+note3.addEventListener("mousedown", (event) => {
+    note3.style.backgroundColor = "yellow";
+    console.log(event);
+    a3.src=`key03.mp3`;
+});
+
+note3.addEventListener("mouseup", (event) => {
+    note3.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note4.addEventListener("mousedown", (event) => {
+    note4.style.backgroundColor = "green";
+    console.log(event);
+    a4.src=`key04.mp3`;  
+});
+
+note4.addEventListener("mouseup", (event) => {
+    note4.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note5.addEventListener("mousedown", (event) => {
+    note5.style.backgroundColor = "blue";
+    console.log(event);
+    a5.src=`key05.mp3`;  
+});
+
+note5.addEventListener("mouseup", (event) => {
+    note5.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note6.addEventListener("mousedown", (event) => {
+    note6.style.backgroundColor = "indigo";
+    console.log(event);
+    a6.src=`key06.mp3`;  
+});
+
+note6.addEventListener("mouseup", (event) => {
+    note6.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note7.addEventListener("mousedown", (event) => {
+    note7.style.backgroundColor = "violet";
+    console.log(event);
+    a7.src=`key07.mp3`;  
+});
+
+note7.addEventListener("mouseup", (event) => {
+    note7.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note8.addEventListener("mousedown", (event) => {
+    note8.style.backgroundColor = "red";
+    console.log(event);
+    a8.src=`key08.mp3`;  
+});
+
+note8.addEventListener("mouseup", (event) => {
+    note8.style.backgroundColor = "white";
+    console.log(event);
+});
+
+;
+note9.addEventListener("mousedown",(event)=>{
+    note9.style.backgroundColor="orange";
+    console.log(event);
+    a9.src=`key09.mp3`; 
+}) 
+
+note9.addEventListener("mouseup",(event)=>{
+    note9.style.backgroundColor="white";
+    console.log(event);
+}) 
+
+note10.addEventListener("mousedown", (event) => {
+    note10.style.backgroundColor = "yellow";
+    console.log(event);
+    a10.src=`key10.mp3`; 
+});
+
+note10.addEventListener("mouseup", (event) => {
+    note10.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note11.addEventListener("mousedown", (event) => {
+    note11.style.backgroundColor = "green";
+    console.log(event);
+    a11.src=`key11.mp3`; 
+});
+
+note11.addEventListener("mouseup", (event) => {
+    note11.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note12.addEventListener("mousedown", (event) => {
+    note12.style.backgroundColor = "blue";
+    console.log(event);
+    a12.src=`key12.mp3`; 
+});
+
+note12.addEventListener("mouseup", (event) => {
+    note12.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note13.addEventListener("mousedown", (event) => {
+    note13.style.backgroundColor = "indigo";
+    console.log(event);
+    a13.src=`key13.mp3`; 
+});
+
+note13.addEventListener("mouseup", (event) => {
+    note13.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note14.addEventListener("mousedown", (event) => {
+    note14.style.backgroundColor = "violet";
+    console.log(event);
+    a14.src=`key14.mp3`; 
+});
+
+note14.addEventListener("mouseup", (event) => {
+    note14.style.backgroundColor = "white";
+    console.log(event);
+    
+});
+
+note15.addEventListener("mousedown", (event) => {
+    note15.style.backgroundColor = "red";
+    console.log(event);
+    a15.src=`key15.mp3`; 
+});
+
+note15.addEventListener("mouseup", (event) => {
+    note15.style.backgroundColor = "white";
+    console.log(event);
+});
+
+
+
+
+note16.addEventListener("mousedown",(event)=>{
+    note16.style.backgroundColor="red";
+    console.log(event);
+    a15.src=`key16.mp3`; 
+}) 
+
+note16.addEventListener("mouseup",(event)=>{
+    note16.style.backgroundColor="white";
+    console.log(event);
+}) 
+note17.addEventListener("mousedown",(event)=>{
+    note17.style.backgroundColor="orange";
+    console.log(event);
+    a15.src=`key17.mp3`; 
+}) 
+
+note17.addEventListener("mouseup",(event)=>{
+    note17.style.backgroundColor="white";
+    console.log(event);
+}) 
+
+note18.addEventListener("mousedown", (event) => {
+    note18.style.backgroundColor = "yellow";
+    console.log(event);
+    a15.src=`key18.mp3`; 
+});
+
+note18.addEventListener("mouseup", (event) => {
+    note18.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note19.addEventListener("mousedown", (event) => {
+    note19.style.backgroundColor = "green";
+    console.log(event);
+    a15.src=`key19.mp3`; 
+});
+
+note19.addEventListener("mouseup", (event) => {
+    note19.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note20.addEventListener("mousedown", (event) => {
+    note20.style.backgroundColor = "blue";
+    console.log(event);
+    a15.src=`key20.mp3`; 
+});
+
+note20.addEventListener("mouseup", (event) => {
+    note20.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note21.addEventListener("mousedown", (event) => {
+    note21.style.backgroundColor = "indigo";
+    console.log(event);
+    a15.src=`key21.mp3`; 
+});
+
+note21.addEventListener("mouseup", (event) => {
+    note21.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note22.addEventListener("mousedown", (event) => {
+    note22.style.backgroundColor = "violet";
+    console.log(event);
+    a15.src=`key22.mp3`; 
+});
+
+note22.addEventListener("mouseup", (event) => {
+    note22.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note23.addEventListener("mousedown", (event) => {
+    note23.style.backgroundColor = "red";
+    console.log(event);
+    a15.src=`key23.mp3`; 
+});
+
+note23.addEventListener("mouseup", (event) => {
+    note23.style.backgroundColor = "white";
+    console.log(event);
+});
+
+;
+note24.addEventListener("mousedown",(event)=>{
+    note24.style.backgroundColor="orange";
+    console.log(event);
+    a15.src=`key24.mp3`; 
+}) 
+
+note24.addEventListener("mouseup",(event)=>{
+    note24.style.backgroundColor="white";
+    console.log(event);
+}) 
+
+note25.addEventListener("mousedown", (event) => {
+    note25.style.backgroundColor = "yellow";
+    console.log(event);
+    a15.src=`key25.mp3`; 
+});
+
+note25.addEventListener("mouseup", (event) => {
+    note25.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note26.addEventListener("mousedown", (event) => {
+    note26.style.backgroundColor = "green";
+    console.log(event);
+    a15.src=`key26.mp3`; 
+});
+
+note26.addEventListener("mouseup", (event) => {
+    note26.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note27.addEventListener("mousedown", (event) => {
+    note27.style.backgroundColor = "blue";
+    console.log(event);
+});
+
+note27.addEventListener("mouseup", (event) => {
+    note27.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note28.addEventListener("mousedown", (event) => {
+    note28.style.backgroundColor = "indigo";
+    console.log(event);
+});
+
+note28.addEventListener("mouseup", (event) => {
+    note28.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note29.addEventListener("mousedown", (event) => {
+    note29.style.backgroundColor = "violet";
+    console.log(event);
+});
+
+note29.addEventListener("mouseup", (event) => {
+    note29.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note30.addEventListener("mousedown", (event) => {
+    note30.style.backgroundColor = "red";
+    console.log(event);
+});
+
+note30.addEventListener("mouseup", (event) => {
+    note30.style.backgroundColor = "white";
+    console.log(event);
+});
+
+
+
+
+
+
+
+
+
+
+
+note31.addEventListener("mousedown",(event)=>{
+    note31.style.backgroundColor="red";
+    console.log(event);
+}) 
+
+note31.addEventListener("mouseup",(event)=>{
+    note31.style.backgroundColor="white";
+    console.log(event);
+}) 
+note32.addEventListener("mousedown",(event)=>{
+    note32.style.backgroundColor="orange";
+    console.log(event);
+}) 
+
+note32.addEventListener("mouseup",(event)=>{
+    note32.style.backgroundColor="white";
+    console.log(event);
+}) 
+
+note33.addEventListener("mousedown", (event) => {
+    note33.style.backgroundColor = "yellow";
+    console.log(event);
+});
+
+note33.addEventListener("mouseup", (event) => {
+    note33.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note34.addEventListener("mousedown", (event) => {
+    note34.style.backgroundColor = "green";
+    console.log(event);
+});
+
+note34.addEventListener("mouseup", (event) => {
+    note34.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note35.addEventListener("mousedown", (event) => {
+    note35.style.backgroundColor = "blue";
+    console.log(event);
+});
+
+note35.addEventListener("mouseup", (event) => {
+    note35.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note36.addEventListener("mousedown", (event) => {
+    note36.style.backgroundColor = "indigo";
+    console.log(event);
+});
+
+note36.addEventListener("mouseup", (event) => {
+    note36.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note37.addEventListener("mousedown", (event) => {
+    note37.style.backgroundColor = "violet";
+    console.log(event);
+});
+
+note37.addEventListener("mouseup", (event) => {
+    note37.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note38.addEventListener("mousedown", (event) => {
+    note38.style.backgroundColor = "red";
+    console.log(event);
+});
+
+note38.addEventListener("mouseup", (event) => {
+    note38.style.backgroundColor = "white";
+    console.log(event);
+});
+
+;
+note39.addEventListener("mousedown",(event)=>{
+    note39.style.backgroundColor="orange";
+    console.log(event);
+}) 
+
+note39.addEventListener("mouseup",(event)=>{
+    note39.style.backgroundColor="white";
+    console.log(event);
+}) 
+
+note40.addEventListener("mousedown", (event) => {
+    note40.style.backgroundColor = "yellow";
+    console.log(event);
+});
+
+note40.addEventListener("mouseup", (event) => {
+    note40.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note41.addEventListener("mousedown", (event) => {
+    note41.style.backgroundColor = "green";
+    console.log(event);
+});
+
+note41.addEventListener("mouseup", (event) => {
+    note41.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note42.addEventListener("mousedown", (event) => {
+    note42.style.backgroundColor = "blue";
+    console.log(event);
+});
+
+note42.addEventListener("mouseup", (event) => {
+    note42.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note43.addEventListener("mousedown", (event) => {
+    note43.style.backgroundColor = "indigo";
+    console.log(event);
+});
+
+note43.addEventListener("mouseup", (event) => {
+    note43.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note44.addEventListener("mousedown", (event) => {
+    note44.style.backgroundColor = "violet";
+    console.log(event);
+});
+
+note44.addEventListener("mouseup", (event) => {
+    note44.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note45.addEventListener("mousedown", (event) => {
+    note45.style.backgroundColor = "red";
+    console.log(event);
+});
+
+note45.addEventListener("mouseup", (event) => {
+    note45.style.backgroundColor = "white";
+    console.log(event);
+});
+
+
+
+
+note46.addEventListener("mousedown",(event)=>{
+    note46.style.backgroundColor="red";
+    console.log(event);
+}) 
+
+note46.addEventListener("mouseup",(event)=>{
+    note46.style.backgroundColor="white";
+    console.log(event);
+}) 
+note47.addEventListener("mousedown",(event)=>{
+    note47.style.backgroundColor="orange";
+    console.log(event);
+}) 
+
+note47.addEventListener("mouseup",(event)=>{
+    note47.style.backgroundColor="white";
+    console.log(event);
+}) 
+
+note48.addEventListener("mousedown", (event) => {
+    note48.style.backgroundColor = "yellow";
+    console.log(event);
+});
+
+note48.addEventListener("mouseup", (event) => {
+    note48.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note49.addEventListener("mousedown", (event) => {
+    note49.style.backgroundColor = "green";
+    console.log(event);
+});
+
+note49.addEventListener("mouseup", (event) => {
+    note49.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note50.addEventListener("mousedown", (event) => {
+    note50.style.backgroundColor = "blue";
+    console.log(event);
+});
+
+note50.addEventListener("mouseup", (event) => {
+    note50.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note51.addEventListener("mousedown", (event) => {
+    note51.style.backgroundColor = "indigo";
+    console.log(event);
+});
+
+note51.addEventListener("mouseup", (event) => {
+    note51.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note52.addEventListener("mousedown", (event) => {
+    note52.style.backgroundColor = "violet";
+    console.log(event);
+});
+
+note52.addEventListener("mouseup", (event) => {
+    note52.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note53.addEventListener("mousedown", (event) => {
+    note53.style.backgroundColor = "red";
+    console.log(event);
+});
+
+note53.addEventListener("mouseup", (event) => {
+    note53.style.backgroundColor = "white";
+    console.log(event);
+});
+
+;
+note54.addEventListener("mousedown",(event)=>{
+    note54.style.backgroundColor="orange";
+    console.log(event);
+}) 
+
+note54.addEventListener("mouseup",(event)=>{
+    note54.style.backgroundColor="white";
+    console.log(event);
+}) 
+
+note55.addEventListener("mousedown", (event) => {
+    note55.style.backgroundColor = "yellow";
+    console.log(event);
+});
+
+note55.addEventListener("mouseup", (event) => {
+    note55.style.backgroundColor = "white";
+    console.log(event);
+});
+
+note56.addEventListener("keydown", (event) => {
+    // note56.style.backgroundColor = "green";
+    console.log(event);
+    console.log(event);
+});
+
+note56.addEventListener("mouseup", (event) => {
+    note56.style.backgroundColor = "white";
+    console.log(event);
+});
+console.log(note56);
+document.addEventListener("keydown",(event)=>{
+    console.log(event.code);
+    if(event.code==="KeyL"){
+        a56.src=`key24.mp3`;
+    }
+})
